9abd30467de475a6c702b2914523272271035501
igribov <imilyagribov@gmail.com>
2017-03-02 20:04:07 +0300

    +alg

--- sort/insertation-sort.py
+++ sort/insertation-sort.py
@@ -1,25 +1,19 @@
 def insertationSort(A, n):
     for i in range(1, n):
         key = A[i] # вытаскиваем текущее значение
-        j = i - 1 # предыдущая позиция        
-        while j >= 0 and A[j] > key: # передвигаем пока пред. больше
-            A[j+1] = A[j]
+        j = i - 1 # предыдущая позиция
+        while j >= 0 :# передвигаем пока предыдущее больше
+            
+            if A[j] > key:
+                A[j+1] = A[j]
+                A[j] = key
             j-=1
-        A[j+1] = key
-    return A
-
-def insertationSort2(A, n):
-    for i in range(0, n):
-        key = A[i] # вытаскиваем текущее значение
-        j = i - 1 # предыдущая позиция        
-        while j > -1 and A[j] > key: # передвигаем пока пред. больше
-            A[j+1] = A[j]
-            j-=1
-        A[j+1] = key
+        
     return A
 
 if __name__ == '__main__':
-    A = [22,8,5,2,100,15,13,16,18,14,11,10,6,5,8,3,2,1,22,43,4]
+    A = [22,8,5,2,100,15,13,16,18,14,11,10,6,5,8,3,1,22,43,4]
+    A = [11,10,9,8,7,6,5,4,3,2,1,12,13,14,15,16,17]
     
     res = insertationSort(A, len(A));
-    print(res, len(res), len(A))
+    print(A)
